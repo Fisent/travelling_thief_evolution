ea00954a5b38cd4551af067b2be845b70ad615dc
fisent <lukzieniewicz@gmail.com>
2019-03-16 02:45:12 +0100

    Build system alignment to work with gtest and gmock that is installed in the system

--- googletest/bin/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fisent/Documents/GeneticAlgorithm/ut/googletest/googlemock
-# Build directory: /home/fisent/Documents/GeneticAlgorithm/ut/googletest/bin/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
